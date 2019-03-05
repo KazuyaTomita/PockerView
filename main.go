29e52c0ad27c85e88234f81026d7a1e2235bb7be
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-05 19:39:27 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// first we wait command from GUI(CUI) server.
+	// After that, we execute something following the command.
+	fmt.Printf("start\n")
+}
+
