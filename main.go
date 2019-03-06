400d423f40c77bbeb2e4e98e715d61ade2c85153
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-06 21:59:14 +0900

    WIP try to ipc through pipe

--- main.go
+++ main.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
-func main() {
-	//
-	var a int
-	fmt.Scan(&a)
-	fmt.Printf("%d\n",a )
-	//
-
-	cmd := exec.Command("wc")
-	stdin, _ := cmd.StdinPipe()
-	fmt.Printf("readしたよ")
-	io.WriteString(stdin, "hogehoge\\0")
-	stdin.Close()
-	fmt.Printf("closeしなかったよ")
-	//?out, _ := cmd.Output()
-	// fmt.Printf("結果: %s", out)
-	fmt.Printf("clofafafaf")
-
+func readStuff(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		fmt.Println("Performed Scan")
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
 
+func main() {
+	cmd := exec.Command("wc")
+	stdin, stdinErr := cmd.StdinPipe()
+	stdout, stdoutErr := cmd.StdoutPipe()
+	if stdinErr != nil || stdoutErr != nil {
+		panic("could not get stdin or stdout")
+	}
+	io.WriteString(stdin, "hogeaaaa ddd")
+	stdin.Close()
+	err := cmd.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to start err=%v", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	fmt.Println("Scanner created")
+
+	defer cmd.Wait()
+
+	go readStuff(scanner)
+}
