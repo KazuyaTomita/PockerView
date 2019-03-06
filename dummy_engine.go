af5bf65564221462d54eb6bc030e84e0904a21ff
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-06 21:32:06 +0900

    WIP try to communicate through pipe

--- dummy_engine.go
+++ dummy_engine.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("sleep 7 && echo 'hoge'")
+	cmd := exec.Command("sleep 5 && echo 'hoge'")
 	stdin, stdinErr := cmd.StdinPipe()
 	stdout, stdoutErr := cmd.StdoutPipe()
 	if stdinErr != nil || stdoutErr != nil {
@@ -15,13 +15,14 @@ func main() {
 	}
 	fmt.Printf("write something\n" )
 	io.WriteString(stdin, "hogeaaaa ddd")
-	buffer := make([]byte,1024)
+	buffer := make([]byte,10)
 	fmt.Printf("read buffer\n" )
 	// text ,_ := stdout.Read(buffer)
 	io.ReadFull(stdout, buffer)
+	cmd.Start()
 	fmt.Printf("print text from the command\n" )
-	fmt.Printf("OUTPUT=%s", buffer[3])
-	//
+	fmt.Printf("OUTPUT=%s", buffer[1])
+
 	//var a int
 	//fmt.Scan(&a)
 	//fmt.Printf("%d\n",a )
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os/exec"
+)
 
 func main() {
 	//
@@ -8,5 +12,16 @@ func main() {
 	fmt.Scan(&a)
 	fmt.Printf("%d\n",a )
 	//
+
+	cmd := exec.Command("wc")
+	stdin, _ := cmd.StdinPipe()
+	fmt.Printf("readしたよ")
+	io.WriteString(stdin, "hogehoge\\0")
+	stdin.Close()
+	fmt.Printf("closeしなかったよ")
+	//?out, _ := cmd.Output()
+	// fmt.Printf("結果: %s", out)
+	fmt.Printf("clofafafaf")
+
 }
 
