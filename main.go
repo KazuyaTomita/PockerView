c8d522c7b4f435256fae7958c085d0e41dffd753
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-12 20:20:00 +0900

    WIP try to use toml as config file

--- main.go
+++ main.go
@@ -3,33 +3,80 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"os"
 	"os/exec"
 )
 
 type Config struct {
 	Server ServerConfig
-	Engines EnginesConfig
+	Engines MultiEnginesConfig
+	Cli CliConfig
 }
 
 type ServerConfig struct {
 	Enable bool
 	Ip  string
 	Port  string
+	Path string
 }
 
-type EnginesConfig struct {
+type MultiEnginesConfig struct {
 	Enable bool
 	number int
-	Paths []EnginePath
+	Path []string
 }
 
-type EnginePath struct {
+
+
+type CliConfig struct {
+	Enable bool
 	Path string
 }
 
 
 func main() {
+	
+	var config Config
+	// read config file
+	_, parseErr := toml.DecodeFile("config.toml", &config)
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	// check whether we can decide which mode is used
+	if (config.Server.Enable && config.Engines.Enable) ||
+		(config.Server.Enable && config.Cli.Enable) ||
+		(config.Cli.Enable && config.Engines.Enable) {
+		panic("not sure of which mode is used. please check config. Only single enable flag should be true.")
+	}
+
+	if config.Server.Enable {
+		// communicate with TCP/IP server
+
+	} else if config.Engines.Enable {
+		// play games with multiple engines used
+
+	} else {
+		// CLI mode
+
+	}
+
+
+
+
+	fmt.Printf("Enable is :%t\n", config.Server.Enable)
+
+	fmt.Printf("multiEngine Enable is :%t\n", config.Engines.Enable)
+	fmt.Printf("Cli Enable is :%t\n", config.Cli.Enable)
+	fmt.Printf("Port is :%s\n", config.Server.Port)
+
+	fmt.Printf("%v\n", config.Server)
+	fmt.Printf("%v\n", config.Engines)
+	fmt.Printf("%v\n", config.Cli)
+
+
+
 	stdin := bufio.NewScanner(os.Stdin)
 
 
