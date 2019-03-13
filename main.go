9b55740db945be38df7faff84c3f2c5e12d31bd9
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-13 10:33:35 +0900

    separate pipe parts

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 )
 
 /*
@@ -35,53 +34,16 @@ func main() {
 	if config.Server.Enable {
 		// communicate with TCP/IP server
 		fmt.Printf("server mode\n")
+		// TODO need to set inputScanner
 
 	} else if config.Engines.Enable {
 		// play games with multiple engines used
 		fmt.Printf("multi-engine mode\n")
-
 	} else {
 		// CLI mode
 		fmt.Printf("cli mode\n")
 		inputScanner = bufio.NewScanner(os.Stdin)
 	}
 
-	// we prepare to execute an engine.
-	cmd := exec.Command(config.Cli.Path)
-	// pipe connecting to the engine's standard output
-	stdoutPipe, stdoutErr := cmd.StdoutPipe()
-	// pipe connecting to the engine's standard input
-	stdinPipe, stdinErr := cmd.StdinPipe()
-
-	if stdinErr != nil || stdoutErr != nil {
-		panic("could not get stdinPipe or stdoutPipe")
-	}
-
-	scanner := bufio.NewScanner(stdoutPipe)
-	var writer = bufio.NewWriter(stdinPipe)
-
-	go writeInput(inputScanner, writer)
-	go printOutput(scanner)
-
-	cmd.Run()
-}
-
-// first argument is used to read input
-// second one is used to write the input which is received through the scanner
-func writeInput(scanner *bufio.Scanner, writer *bufio.Writer) {
-	for scanner.Scan() {
-		line := scanner.Text()
-		writer.WriteString(line)
-		// newline code is really important when sending messages
-		writer.WriteString("\n")
-		writer.Flush()
-	}
-}
-
-// receive output and print it
-func printOutput(scanner *bufio.Scanner) {
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-	}
+	ConnectEngine(inputScanner, config.Cli.Path)
 }
--- pipe.go
+++ pipe.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+)
+
+// first argument is used to read input
+// second one is used to write the input which is received through the scanner
+func ConnectEngine(inputScanner *bufio.Scanner, path string) {
+	// we prepare to execute an engine.
+	cmd := exec.Command(path)
+	// pipe connecting to the engine's standard output
+	stdoutPipe, stdoutErr := cmd.StdoutPipe()
+	// pipe connecting to the engine's standard input
+	stdinPipe, stdinErr := cmd.StdinPipe()
+
+	if stdinErr != nil || stdoutErr != nil {
+		panic("could not get stdinPipe or stdoutPipe")
+	}
+
+	scanner := bufio.NewScanner(stdoutPipe)
+	var writer = bufio.NewWriter(stdinPipe)
+
+	go writeInput(inputScanner, writer)
+	go printOutput(scanner)
+
+	cmd.Run()
+}
+
+// first argument is used to read input
+// second one is used to write the input which is received through the scanner
+func writeInput(scanner *bufio.Scanner, writer *bufio.Writer) {
+	for scanner.Scan() {
+		line := scanner.Text()
+		writer.WriteString(line)
+		// newline code is really important when sending messages
+		writer.WriteString("\n")
+		writer.Flush()
+	}
+}
+
+// receive output and print it
+func printOutput(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
