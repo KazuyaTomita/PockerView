ff08318d9b26e608a4d682213d394773503b9f2c
Kazuya Tomita <iamtommy1125@icloud.com>
2019-03-16 17:12:33 +0900

    comment out for build in container

--- dummy_engine.go
+++ dummy_engine.go
@@ -1,20 +1,20 @@
 package main
-
-import (
-	"bufio"
-	"os"
-	"fmt"
-)
-
-func main() {
-	stdin := bufio.NewScanner(os.Stdin)
-	for stdin.Scan() {
-		switch stdin.Text() {
-		case "is_ready":
-			fmt.Println("is_ready ok!")
-		default:
-			fmt.Println("other")
-		}
-	}
-}
-
+//
+//import (
+//	"bufio"
+//	"os"
+//	"fmt"
+//)
+//
+//func main() {
+//	stdin := bufio.NewScanner(os.Stdin)
+//	for stdin.Scan() {
+//		switch stdin.Text() {
+//		case "is_ready":
+//			fmt.Println("is_ready ok!")
+//		default:
+//			fmt.Println("other")
+//		}
+//	}
+//}
+//
